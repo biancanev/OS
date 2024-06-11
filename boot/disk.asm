79af2e7e454c50dfffa2ad8a45eb25ba997a68e9
Ryan Kwong <bebostunner234@gmail.com>
2024-06-11 15:04:44 -0700

    Update disk.asm

--- boot/disk.asm
+++ boot/disk.asm
@@ -3,12 +3,16 @@ disk_load:
     mov ah, 0x02
     mov al, dh
     mov ch, 0x00
+    mov dh, 0x00
     mov cl, 0x02
+
     int 0x13
+
     jc disk_error
     pop dx
     cmp dh, al
     jne disk_error
+    call disk_successful
     ret
 
 disk_error:
@@ -19,4 +23,10 @@ disk_error:
     call print_hex
     jmp $
 
+disk_successful:
+    mov bx, disk_success_msg
+    call print_string
+    ret
+
 disk_error_msg: db "Disk read error: ", 0
+disk_success_msg: db "Disk successfully read...", 0xA, 0xD, 0
